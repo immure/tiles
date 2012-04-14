bb4e72c0d9ae23f58958b73acb7f03840748231b
Mike Hepple <mike@impresariostudios.com>
2012-04-14 22:00:36 +0100

    initial

--- tiles/addons/facebook_tiles_old.py
+++
@@ -1,8 +0,0 @@
-import pprint
-import facebook
-
-oauth_access_token='AAAEASRPSx7oBADFK6uKeNZAYLQZAMPyK5m9X9vtqkC2ZA2PDndDz00onDAQ7OPFdGrZCPANsDdFZBffqAgXVPj37ny5XpyohBCkmHHQjNJwZDZD'
-graph = facebook.GraphAPI(oauth_access_token)
-profile = graph.get_object("me/inbox")
-pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(profile)
